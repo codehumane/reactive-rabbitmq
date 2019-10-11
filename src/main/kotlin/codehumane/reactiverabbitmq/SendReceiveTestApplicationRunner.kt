cb8bfcdd715dcb1cc758ed0a160448f8feba9e2e
codehumane <codehumane@gmail.com>
2019-10-11 15:30:02 -0500

    StopWatch(to calculate total time) & CountDownLatch(wait enough time to be done) add

--- src/main/kotlin/codehumane/reactiverabbitmq/SendReceiveTestApplicationRunner.kt
+++ src/main/kotlin/codehumane/reactiverabbitmq/SendReceiveTestApplicationRunner.kt
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
+import org.springframework.util.StopWatch
+import reactor.core.Disposable
 import reactor.core.publisher.Flux
 import reactor.rabbitmq.OutboundMessage
 import reactor.rabbitmq.Sender
-import java.time.Duration
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 @Component
@@ -20,22 +22,49 @@ class SendReceiveTestApplicationRunner(
 ) : ApplicationRunner {
 
     private val log = LoggerFactory.getLogger(SendReceiveTestApplicationRunner::class.java)
+    private val messageCount = 1_000
 
     override fun run(args: ApplicationArguments?) {
+        calculateTime {
+            waitTaskDone {
+                subscribeMessages(it)
+                sendMessages()
+            }
+        }
+    }
 
-        deliveryFlux
-            .subscribe { log.info("received: ${String(it.body)}") }
+    private fun subscribeMessages(latch: CountDownLatch) {
+        deliveryFlux.subscribe {
+            log.info("received: ${String(it.body)}")
+            latch.countDown()
+        }
+    }
 
+    private fun sendMessages(): Disposable {
         val messages = Flux
-            .range(0, 10)
-            .delayElements(Duration.ofMillis(100))
+            .range(1, messageCount)
+            // .delayElements(Duration.ofMillis(100))
             .map { OutboundMessage("", queueName, "message-$it".toByteArray()) }
 
-        sender
+        return sender
             .send(messages)
             .subscribe()
-
-        TimeUnit.SECONDS.sleep(10)
     }
-    
+
+    private fun waitTaskDone(function: (CountDownLatch) -> Unit) {
+        val latch = CountDownLatch(messageCount)
+        function.invoke(latch)
+        latch.await(10, TimeUnit.SECONDS)
+    }
+
+    fun calculateTime(function: () -> Unit) {
+        val stopWatch = StopWatch()
+        stopWatch.start()
+
+        // real function
+        function.invoke()
+
+        stopWatch.stop()
+        log.info("runner took ${stopWatch.totalTimeMillis}ms")
+    }
 }
